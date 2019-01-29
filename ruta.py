e927ac528e04d213e95eb2e975ba408f0414a10c
afelHipHop <afelrh@hotmail.com>
2019-01-29 10:22:12 -0500

    Camino plano guardando la ultima casilla

--- ruta.py
+++ ruta.py
@@ -23,55 +23,63 @@ def inicio(mapa, x, y):
 		return inicio(mapa,x+1,0)
 	else:
 		return "No se encontró inicio"
+		
 
-"""def fin(mapa, x, y):
+def fin(mapa, x, y):
 	if mapa[x][y]=='F':
 		return (x, y)
 	elif y < (len(mapa[x])-1):
-		return inicio(mapa,x,y+1)
+		return fin(mapa,x,y+1)
 	elif x < (len(mapa)-1):
-		return inicio(mapa,x+1,0)
+		return fin(mapa,x+1,0)
 	else:
 		return "No se encontró fin"
-	"""
+	
 
-def ruta(mapa, ini, ant):
+def ruta(mapa, ini, ant, rec):
 	print(mapa[ini[0]][ini[1]])
 	print(mapa[ini[0]][ini[1]-1])
 	print(mapa[ini[0]][ini[1]+1])
 	print(mapa[ini[0]-1][ini[1]])
 	print(mapa[ini[0]+1][ini[1]])
+	print(rec)
 	if mapa[ini[0]][ini[1]]=='F':
 		print("primer if")
 		print(ini)
+		print(rec)
 		return True
 	#Movimiento hacia la izquierda
 	elif (mapa[ini[0]][ini[1]-1]=='0' or mapa[ini[0]][ini[1]-1]=='F') and (ini[0],ini[1]-1) != ant:
 		print("segundo if")
 		print(ini)
-		return ruta(mapa, (ini[0],ini[1]-1), ini)
+		rec.append(ini)
+		return ruta(mapa, (ini[0],ini[1]-1), ini, rec)
 	#Movimiento hacia la derecha
 	elif (mapa[ini[0]][ini[1]+1]=='0' or mapa[ini[0]][ini[1]+1]=='F') and (ini[0],ini[1]+1) != ant:
 		print("tercer if")
 		print(ini)
-		return ruta(mapa, (ini[0],ini[1]+1), ini)
+		rec.append(ini)
+		return ruta(mapa, (ini[0],ini[1]+1), ini, rec)
 	#Movimiento hacia arriba
 	elif (mapa[ini[0]-1][ini[1]]=='0' or mapa[ini[0]-1][ini[1]]=='F') and (ini[0]-1,ini[1]) != ant:
 		print("cuarto if")
 		print(ini)
-		return ruta(mapa, (ini[0]-1,ini[1]), ini)
+		rec.append(ini)
+		return ruta(mapa, (ini[0]-1,ini[1]), ini, rec)
 	#Movimiento hacia abajo
 	elif (mapa[ini[0]+1][ini[1]]=='0' or mapa[ini[0]+1][ini[1]]=='F') and (ini[0]+1,ini[1]) != ant:
 		print("quinto if")
 		print(ini)
-		return ruta(mapa, (ini[0]+1,ini[1]), ini)
+		rec.append(ini)
+		return ruta(mapa, (ini[0]+1,ini[1]), ini, rec)
 	else:
 		print("else")
 		print(ini)
+		print(rec)
 		return False
 
 
 #print(inicio(cargarMapa('lab.txt'),0,0))
-print(ruta(cargarMapa('lab.txt'),inicio(cargarMapa('lab.txt'),0,0),(0,0)))
+print(ruta(cargarMapa('lab.txt'),inicio(cargarMapa('lab.txt'),0,0),(0,0),[]))
 
 
